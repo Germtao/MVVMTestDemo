b44c12afb1da47046297c793591db3f34eb9016d
QDSG <piftao1127@163.com>
2019-12-03 14:15:46 +0800

    Initial Commit

---
+++ MVVMTestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMTestDemo
+//
+//  Created by QDSG on 2019/12/3.
+//  Copyright © 2019 unitTao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
